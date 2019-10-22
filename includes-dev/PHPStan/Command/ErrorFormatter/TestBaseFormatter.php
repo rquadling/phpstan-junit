ff5612a5862a77c994daea1c8f723fbcef94ebdd
Richard Quadling <Richard.Quadling@DigiTickets.co.uk>
2019-10-22 12:17:19 +0100

    Refactored the PHPStan classes to use the PHPStanVendor namespace.
    Moved SimpleRelativePathHelper out of the includes-dev to includes as it
    does not exist in PHPStan-shim and so we need it live

--- includes-dev/PHPStan/Command/ErrorFormatter/TestBaseFormatter.php
+++ includes-dev/PHPStan/Command/ErrorFormatter/TestBaseFormatter.php
@@ -4,100 +4,99 @@ declare(strict_types=1);
 
 namespace PHPStan\Command\ErrorFormatter;
 
+use Exception;
 use PHPStan\Analyser\Error;
 use PHPStan\Command\AnalysisResult;
 use PHPStan\Command\ErrorsConsoleStyle;
-use Symfony\Component\Console\Input\StringInput;
-use Symfony\Component\Console\Output\StreamOutput;
+use PHPStan\ShouldNotHappenException;
+use PHPStan\Testing\TestCase;
+use PHPStanVendor\Symfony\Component\Console\Input\StringInput;
+use PHPStanVendor\Symfony\Component\Console\Output\StreamOutput;
 
-if (!class_exists(TestBaseFormatter::class)) {
-    abstract class TestBaseFormatter extends \PHPStan\Testing\TestCase
+abstract class TestBaseFormatter extends TestCase
+{
+    const DIRECTORY_PATH = '/data/folder/with space/and unicode 😃/project';
+
+    /** @var StreamOutput */
+    private $outputStream;
+
+    /** @var ErrorsConsoleStyle */
+    private $errorConsoleStyle;
+
+    protected function setUp()
     {
-        const DIRECTORY_PATH = '/data/folder/with space/and unicode 😃/project';
+        parent::setUp();
 
-        /** @var StreamOutput */
-        private $outputStream;
-
-        /** @var ErrorsConsoleStyle */
-        private $errorConsoleStyle;
-
-        protected function setUp()
-        {
-            parent::setUp();
-
-            $resource = fopen('php://memory', 'w', false);
-            if ($resource === false) {
-                throw new \PHPStan\ShouldNotHappenException();
-            }
-
-            $this->outputStream = new StreamOutput($resource);
-
-            $this->errorConsoleStyle = new ErrorsConsoleStyle(new StringInput(''), $this->outputStream);
+        $resource = fopen('php://memory', 'w', false);
+        if ($resource === false) {
+            throw new ShouldNotHappenException();
         }
 
-        protected function getOutputContent(): string
-        {
-            rewind($this->outputStream->getStream());
+        $this->outputStream = new StreamOutput($resource);
 
-            $contents = stream_get_contents($this->outputStream->getStream());
-            if ($contents === false) {
-                throw new \PHPStan\ShouldNotHappenException();
-            }
-
-            return $this->rtrimMultiline($contents);
-        }
-
-        protected function getErrorConsoleStyle(): ErrorsConsoleStyle
-        {
-            return $this->errorConsoleStyle;
-        }
-
-        protected function getAnalysisResult(int $numFileErrors, int $numGenericErrors): AnalysisResult
-        {
-            if ($numFileErrors > 4 || $numFileErrors < 0 || $numGenericErrors > 2 || $numGenericErrors < 0) {
-                throw new \Exception();
-            }
-
-            $fileErrors = array_slice(
-                [
-                    new Error('Foo', self::DIRECTORY_PATH.'/folder with unicode 😃/file name with "spaces" and unicode 😃.php', 4),
-                    new Error('Foo', self::DIRECTORY_PATH.'/foo.php', 1),
-                    new Error('Bar', self::DIRECTORY_PATH.'/foo.php', 5),
-                    new Error('Bar', self::DIRECTORY_PATH.'/folder with unicode 😃/file name with "spaces" and unicode 😃.php', 2),
-                ],
-                0,
-                $numFileErrors
-            );
-
-            $genericErrors = array_slice(
-                [
-                    'first generic error',
-                    'second generic error',
-                ],
-                0,
-                $numGenericErrors
-            );
-
-            return new AnalysisResult(
-                $fileErrors,
-                $genericErrors,
-                false,
-                self::DIRECTORY_PATH
-            );
-        }
-
-        private function rtrimMultiline(string $output): string
-        {
-            $result = array_map(
-                static function (string $line): string {
-                    return rtrim($line, " \r\n");
-                },
-                explode("\n", $output)
-            );
-
-            return implode("\n", $result);
-        }
+        $this->errorConsoleStyle = new ErrorsConsoleStyle(new StringInput(''), $this->outputStream);
     }
 
-    return TestBaseFormatter::class;
+    protected function getOutputContent(): string
+    {
+        rewind($this->outputStream->getStream());
+
+        $contents = stream_get_contents($this->outputStream->getStream());
+        if ($contents === false) {
+            throw new ShouldNotHappenException();
+        }
+
+        return $this->rtrimMultiline($contents);
+    }
+
+    protected function getErrorConsoleStyle(): ErrorsConsoleStyle
+    {
+        return $this->errorConsoleStyle;
+    }
+
+    protected function getAnalysisResult(int $numFileErrors, int $numGenericErrors): AnalysisResult
+    {
+        if ($numFileErrors > 4 || $numFileErrors < 0 || $numGenericErrors > 2 || $numGenericErrors < 0) {
+            throw new Exception();
+        }
+
+        $fileErrors = array_slice(
+            [
+                new Error('Foo', self::DIRECTORY_PATH.'/folder with unicode 😃/file name with "spaces" and unicode 😃.php', 4),
+                new Error('Foo', self::DIRECTORY_PATH.'/foo.php', 1),
+                new Error('Bar', self::DIRECTORY_PATH.'/foo.php', 5),
+                new Error('Bar', self::DIRECTORY_PATH.'/folder with unicode 😃/file name with "spaces" and unicode 😃.php', 2),
+            ],
+            0,
+            $numFileErrors
+        );
+
+        $genericErrors = array_slice(
+            [
+                'first generic error',
+                'second generic error',
+            ],
+            0,
+            $numGenericErrors
+        );
+
+        return new AnalysisResult(
+            $fileErrors,
+            $genericErrors,
+            false,
+            self::DIRECTORY_PATH
+        );
+    }
+
+    private function rtrimMultiline(string $output): string
+    {
+        $result = array_map(
+            static function (string $line): string {
+                return rtrim($line, " \r\n");
+            },
+            explode("\n", $output)
+        );
+
+        return implode("\n", $result);
+    }
 }
--- includes/PHPStan/File/RelativeFileHelper.php
+++ includes/PHPStan/File/RelativeFileHelper.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace PHPStan\File;
 
-if (!interface_exists(RelativePathHelper::class)) {
-    interface RelativePathHelper
-    {
-        public function getRelativePath(string $filename): string;
-    }
-
-    return RelativePathHelper::class;
+interface RelativePathHelper
+{
+    public function getRelativePath(string $filename): string;
 }
